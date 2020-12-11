7b718eb9774cb51a3e89214ff777452fe3b114fa
Marco Espinoza <marco_a_espinoza@yahoo.com>
2020-12-10 22:59:49 -0800

    Setup TabView

---
+++ Appetizers/AppetizerListView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct AppetizerListView: View {
+    var body: some View {
+        Text("Appetizer List View")
+    }
+}
+
+struct ApptizersListView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppetizerListView()
+    }
+}
