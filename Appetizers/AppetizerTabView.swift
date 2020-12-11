4a8d6c0e9439bcb507e265d32e8443e6e989d149
Marco Espinoza <marco_a_espinoza@yahoo.com>
2020-12-10 21:58:10 -0800

    Change content view to TabView

---
+++ Appetizers/AppetizerTabView.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+struct AppetizerTabView: View {
+    var body: some View {
+        TabView {
+            Text("Hello, world!")
+                .padding()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppetizerTabView()
+    }
+}
