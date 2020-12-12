55b4757f72c789feda751069501d4e50ec0552f9
Marco Espinoza <marco_a_espinoza@yahoo.com>
2020-12-12 11:13:04 -0800

    Set network call to download images

--- Appetizers/NetworkManager.swift
+++ Appetizers/NetworkManager.swift
@@ -1,7 +1,9 @@
-import Foundation
+import UIKit
 
 final class NetworkManager {
+    
     static let shared = NetworkManager()
+    private let cache = NSCache<NSString, UIImage>()
     
     private init() {}
     
@@ -41,6 +43,32 @@ final class NetworkManager {
             } catch {
                 completed(.failure(.invalidData))
             }
+        }
+        task.resume()
+    }
+    
+    func downloadImage(fromURLString urlString: String, completed: @escaping (UIImage?) -> Void ) {
+        
+        let cacheKey = NSString(string: urlString)
+        
+        if let image = cache.object(forKey: cacheKey) {
+            completed(image)
+            return
+        }
+        
+        guard let url = URL(string: urlString) else {
+            completed(nil)
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, error in
+            guard let data = data, let image = UIImage(data: data) else {
+                completed(nil)
+                return
+            }
+            
+            self.cache.setObject(image, forKey: cacheKey)
+            completed(image)
         }
         task.resume()
     }
