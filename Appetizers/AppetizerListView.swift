39b3d863a2837f4455e9a1fe2e5ff87bdbc79b17
Marco Espinoza <marco_a_espinoza@yahoo.com>
2020-12-11 17:56:30 -0800

    Refactor AppetizerListCell

--- Appetizers/AppetizerListView.swift
+++ Appetizers/AppetizerListView.swift
@@ -4,24 +4,7 @@ struct AppetizerListView: View {
     var body: some View {
         NavigationView {
             List(MockData.appetizers) { appetizer in
-                HStack {
-                    Image("asian-flank-steak")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 120, height: 90)
-                        .cornerRadius(8)
-                    
-                    VStack(alignment: .leading, spacing: 5) {
-                        Text(appetizer.name)
-                            .font(.title2)
-                            .fontWeight(.medium)
-                        
-                        Text("$\(appetizer.price, specifier: "%.2f")")
-                            .foregroundColor(.secondary)
-                            .fontWeight(.semibold)
-                    }
-                    .padding(.leading)
-                }
+                AppetizerListCell(appetizer: appetizer)
             }
                 .navigationTitle("🍘 Appetizers")
         }
