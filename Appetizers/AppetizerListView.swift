98ede337b176ab65b2d3a359c66ca4aeb89da68e
Marco Espinoza <marco_a_espinoza@yahoo.com>
2020-12-11 17:45:02 -0800

    Set mock data and test mock data in ListView

--- Appetizers/AppetizerListView.swift
+++ Appetizers/AppetizerListView.swift
@@ -3,7 +3,26 @@ import SwiftUI
 struct AppetizerListView: View {
     var body: some View {
         NavigationView {
-            Text("Appetizers")
+            List(MockData.appetizers) { appetizer in
+                HStack {
+                    Image("asian-flank-steak")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 120, height: 90)
+                        .cornerRadius(8)
+                    
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text(appetizer.name)
+                            .font(.title2)
+                            .fontWeight(.medium)
+                        
+                        Text("$\(appetizer.price, specifier: "%.2f")")
+                            .foregroundColor(.secondary)
+                            .fontWeight(.semibold)
+                    }
+                    .padding(.leading)
+                }
+            }
                 .navigationTitle("🍘 Appetizers")
         }
     }
